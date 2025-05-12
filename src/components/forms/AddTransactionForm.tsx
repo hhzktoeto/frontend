56ed1a25f69d4e709a5a63f2f67bb1ca81917cf4
Dmitriy Goncharov <incoom@yandex.ru>
2025-05-12 18:54:06 +0300

    FEAT-unstable: adding transaction works

--- src/components/forms/AddTransactionForm.tsx
+++ src/components/forms/AddTransactionForm.tsx
@@ -1,8 +1,7 @@
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import type {Category} from "../../types/Category.ts";
 import {addTransaction} from "../../services/transactionsService.ts";
 import {getCategories} from "../../services/categoryService.ts";
-import * as React from "react";
 
 export interface TransactionForm {
     type: string;
@@ -12,38 +11,19 @@ export interface TransactionForm {
     description: string;
 }
 
-const emptyTransactionForm: TransactionForm = {
-    type: "EXPENSE",
-    category: "",
-    amount: 0.00,
-    date: new Date().toISOString().split("T")[0],
-    description: ""
-}
-
 export default function AddTransactionForm() {
+    const typeRef = useRef<HTMLSelectElement>(null)
+    const categoryRef = useRef<HTMLSelectElement>(null)
+    const amountRef = useRef<HTMLInputElement>(null)
+    const dateRef = useRef<HTMLInputElement>(null)
+    const descriptionRef = useRef<HTMLInputElement>(null)
     const [categories, setCategories] = useState<Category[]>([]);
-    const [transactionForm, setTransactionForm] = useState<TransactionForm>(emptyTransactionForm)
-    const handleFormChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-        const {name, value} = e.target;
-        setTransactionForm(prevState => {
-            let processedValue: string | number = value;
-
-            if (name === "amount") {
-                processedValue = Number(value) || 0.00;
-            }
-            if (name === "date") {
-                processedValue = new Date(value).toISOString().split("T")[0];
-            }
-
-            return {
-                ...prevState,
-                [name]: processedValue
-            }
-        })
-    }
 
     useEffect(() => {
-        getCategories().then(setCategories)
+        getCategories().then(setCategories);
+        if (dateRef.current) {
+            dateRef.current.value = new Date().toISOString().split("T")[0];
+        }
     }, []);
 
     return (
@@ -51,23 +31,20 @@ export default function AddTransactionForm() {
             <div className="p-4 border-t bg-gray-50">
                 <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
                     <div className="md:col-span-4">
-                        <h3 className="text-lg font-medium mb-2">Добавить транзакцию</h3>
-                        <div className="grid grid-cols-1 md:grid-cols-4 gap-3">
+                        <h3 className="text-lg font-medium mb-4">Добавить транзакцию</h3>
+                        <div className="grid grid-cols-1 md:grid-cols-4 gap-2">
                             <div>
                                 <label className="block text-xs text-gray-500 mb-1">Тип</label>
-                                <select
-                                    name="type"
-                                    value={transactionForm.type}
-                                    onChange={handleFormChange}
-                                    className="w-full p-2 border rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-primary-400">
+                                <select ref={typeRef}
+                                        className="w-full p-2 border rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-primary-400">
                                     <option>Расход</option>
                                     <option>Доход</option>
                                 </select>
                             </div>
                             <div>
                                 <label className="block text-xs text-gray-500 mb-1">Категория</label>
-                                <select
-                                    className="w-full p-2 border rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-primary-400">
+                                <select ref={categoryRef}
+                                        className="w-full p-2 border rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-primary-400">
                                     {
                                         categories.map(category => {
                                             return <option key={category.id}>{category.name}</option>
@@ -77,19 +54,22 @@ export default function AddTransactionForm() {
                             </div>
                             <div>
                                 <label className="block text-xs text-gray-500 mb-1">Сумма</label>
-                                <input type="number"
+                                <input ref={amountRef}
+                                       type="number"
                                        className="w-full p-2 border rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-primary-400"
                                        placeholder="0.00"/>
                             </div>
                             <div>
                                 <label className="block text-xs text-gray-500 mb-1">Дата</label>
-                                <input type="date"
+                                <input ref={dateRef}
+                                       type="date"
                                        className="w-full p-2 border rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-primary-400"
                                        placeholder="0.00"/>
                             </div>
                             <div className="md:col-span-4">
                                 <label className="block text-xs text-gray-500 mb-1">Описание</label>
-                                <input type="text"
+                                <input ref={descriptionRef}
+                                       type="text"
                                        className="w-full p-2 border rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-primary-400"/>
                             </div>
                         </div>
@@ -101,8 +81,13 @@ export default function AddTransactionForm() {
                         onClick={
                             async () => {
                                 try {
-                                    await addTransaction(transactionForm);
-                                    setTransactionForm(emptyTransactionForm)
+                                    await addTransaction({
+                                        type: String(typeRef.current?.value),
+                                        category: String(categoryRef.current?.value),
+                                        amount: Number(amountRef.current?.value),
+                                        date: String(dateRef.current?.value),
+                                        description: String(descriptionRef.current?.value)
+                                    });
                                 } catch (error) {
                                     console.error("Ошибка во время добавления транзакции:", error)
                                 }
