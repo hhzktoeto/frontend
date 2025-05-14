0d8f820489177083c7e5a1c206a517a173007339
Dmitriy Goncharov <incoom@yandex.ru>
2025-05-14 21:03:20 +0300

    FEAT-unstable: made balance card updatable

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <header className="mb-8">
-            <h1 className="text-3xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-primary-600 to-primary-800">Money Manager</h1>
+            <h1 className="text-3xl font-bold bg-clip-text bg-gradient-to-r text-primary-600">Money Manager</h1>
             <p className="text-gray-600 mt-2">Трекер личных финансов</p>
         </header>
     );
--- src/components/cards/CurrentBalance.tsx
+++ src/components/cards/CurrentBalance.tsx
@@ -1,4 +1,18 @@
+import {useTransactionsQuery} from "../../hooks/useTransactionHook";
+import {useMemo} from "react";
+
 export default function CurrentBalance() {
+
+    const {data: transactions = []} = useTransactionsQuery();
+
+    const totalBalance = useMemo(() => {
+        return transactions.reduce((sum, transaction) => {
+            return transaction.type === "INCOME"
+                ? sum + transaction.amount
+                : sum - transaction.amount;
+        }, 0);
+    }, [transactions]);
+
     return (
         <div
             className="bg-white p-5 rounded-xl shadow-md hover:shadow-lg transition-all duration-300 transform hover:-translate-y-1">
@@ -6,7 +20,7 @@ export default function CurrentBalance() {
                 <h2 className="text-xl font-semibold">Текущий баланс</h2>
                 <span className="material-symbols-outlined text-primary-500">account_balance_wallet</span>
             </div>
-            <p className="text-3xl font-bold mb-2">$8,459.52</p>
+            <p className="text-3xl font-bold mb-2">{totalBalance}</p>
             <div className="flex items-center text-green-500">
                 <span className="material-symbols-outlined text-sm mr-1">trending_up</span>
                 <span className="text-sm">+2.4% from last month</span>
