aa433254fa0a0806f1d4d2fb93b550481e3f9b13
Dmitriy Goncharov <ritm.dgoncharov@gmail.com>
2025-05-20 16:32:22 +0300

    FIX: removed hardcoded localhost server address

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -5,31 +5,29 @@ import {ApiPath} from '../constants/api-path';
 
 @Injectable({providedIn: "root"})
 export class ApiService {
-    private readonly baseUrl = "http://localhost:8080";
     private http = inject(HttpClient);
 
     async getAll<T>(path: ApiPath): Promise<T[]> {
         return firstValueFrom(
-            this.http.get<T[]>(this.baseUrl.concat(path))
+            this.http.get<T[]>(path)
         );
     }
 
     async add<TRequest, TResponse>(path: ApiPath, data: TRequest): Promise<TResponse> {
         return firstValueFrom(
-            this.http.post<TResponse>(this.baseUrl.concat(path), data)
+            this.http.post<TResponse>(path, data)
         );
     }
 
     async update<T>(path: ApiPath, data: T): Promise<T> {
         return firstValueFrom(
-            this.http.put<T>(this.baseUrl.concat(path), data)
+            this.http.put<T>(path, data)
         );
     }
 
     async delete(path: ApiPath, id: number): Promise<void> {
-        const url = this.baseUrl.concat(path).concat("/").concat(id.toString());
         return firstValueFrom(
-            this.http.delete<void>(url)
+            this.http.delete<void>(path.concat("/").concat(id.toString()))
         );
     }
 }
